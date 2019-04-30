b9d16ce31055e9f2f0a5d495a9657c55a90c47c0
Serge Schneider <serge@raspberrypi.org>
2019-04-30 12:13:49 +0100

    Add message if WiFi is blocked

---
+++ etc/profile.d/wifi-check.sh
@@ -0,0 +1,18 @@
+(
+        export TEXTDOMAIN=wifi-check
+
+        . gettext.sh
+
+        if [ ! -x /usr/sbin/rfkill ]; then
+                exit 0
+        fi
+
+        if ! /usr/sbin/rfkill list wifi | grep -q "Soft blocked: yes" ; then
+                exit 0
+        fi
+
+        echo
+        /usr/bin/gettext -s "Wi-Fi is currently blocked by rfkill."
+        /usr/bin/gettext -s "Use raspi-config to set the country before use."
+        echo
+)
