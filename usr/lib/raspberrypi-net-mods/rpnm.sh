6b261f2ba6ec31293a462dbc78e979e58f4d6afb
andrum99 <andrum99@gmail.com>
2020-09-21 22:04:53 +0000

    Move shell script to separate file

---
+++ usr/lib/raspberrypi-net-mods/rpnm.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+if [ -f "/boot/wpa_supplicant.conf" ]; then 
+  /bin/mv /boot/wpa_supplicant.conf /etc/wpa_supplicant/wpa_supplicant.conf
+  if [ -f "/boot/wpa_supplicant.conf.txt" ]; then 
+    /bin/rm /boot/wpa_supplicant.conf.txt
+  fi
+elif [ -f "/boot/wpa_supplicant.conf.txt" ]; then 
+  /bin/mv /boot/wpa_supplicant.conf.txt /etc/wpa_supplicant/wpa_supplicant.conf
+fi
